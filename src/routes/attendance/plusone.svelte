b1c83887a69ea2632d0716f872fe7568a1d7e533
burtonrj <burtonrossj@gmail.com>
2023-06-04 22:17:12 +0100

    Fixed plusone checkbox error

--- src/routes/attendance/plusone.svelte
+++ src/routes/attendance/plusone.svelte
@@ -32,7 +32,7 @@
     <div class="flex flex-col items-center col-span-2">
         <p class="text-center">Will you be bringing a plus one? (Please feel free too!)</p>
         <div class="flex items-center">
-            <input id="link-checkbox" type="checkbox" value={$plusOne} on:change={handlePlusOne} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+            <input id="link-checkbox" type="checkbox" checked={$plusOne} on:change={handlePlusOne} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
             <label for="link-checkbox" class="ml-2 text-md text-gray-900">Yes, I will be bringing a plus one.</label>
         </div>
     </div>
